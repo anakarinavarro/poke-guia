711bc139b5bb3fbb3432d566761ef919caa7e059
ana navarro <anakarina.stratus@gmail.com>
2022-05-13 15:55:39 -0400

    poke-guia

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,118 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <section class="banner">
+      <img src="assets/logo-pokemon.png" />
+    </section>
+
+    <section class="search">
+      <h4>PokeGuía</h4>
+      <form class="input-search" @submit.prevent="findPokemon(pokemonName)">
+       
+        <input type="text" v-model="pokemonName" required />
+        <button type="submit">Buscar</button>
+      </form>
+    </section>
+
+    <section>
+       <p>Nombre: {{pokemonName}} </p> 
+      <img :src="pokemonFrontDefault" alt="" />
+    </section>
+
+    <section class="movements">
+      <h4>Movimientos</h4>
+      <ul>
+        <li v-for="(move, $index) in pokemonMoves" :key="$index">
+          {{ move }}
+        </li>
+      </ul>
+    </section>
+
+    <section class="abilities">
+      <h4>Habilidades</h4>
+      <ul>
+        <li v-for="(ability, $index) in pokemonAbilities" :key="$index">
+          {{ ability }}
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: ()=>({
+    pokemon : null,
+    pokemonName : "pikachu",
+   
+  }),
+  mounted() {
+    this.findPokemon("pikachu")
+  },
+  computed:{
+    pokemonMoves() {
+      return this.pokemon?.moves?.map?.((move) => move.move.name) ?? []
+    },
+    pokemonAbilities() {
+      return this.pokemon?.abilities?.map?.((ability) => ability.ability.name) ?? []
+    },
+    pokemonFrontDefault() {
+      return this.pokemon?.sprites?.front_default ?? ""
+    },
+
+  },
+
+  methods:{
+      async findPokemon(pokemonName) {
+        const pokemon = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+        )
+        .then((response) => response.json().catch((err)=>{
+          alert(`El pokemon ${this.pokemonName} no ha sido encontrado`)
+        }))
+        this.pokemon = pokemon
+        
+        
+      },
+    },
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: black;
+  color: white;
 }
+main {
+  max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+.logo{
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+.search {
+  text-align: center;
+}
+.search .input-search {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+}
+.search .input-search p {
+  margin: 0;
+}
+
 </style>
